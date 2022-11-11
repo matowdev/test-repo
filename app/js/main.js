31872411a7eff59ab908339f190f4d403d3172f6
SiarheiS2K <79189126+SiarheiS2K@users.noreply.github.com>
2022-11-11 20:54:36 +0300

    initial

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  let showPopup = function () {
+    $('.popup-container').css('display', 'flex');
+  };
+
+  let closePopup = function () {
+    $('.popup-container').css('display', 'none');
+  };
+
+  let clearForm = function () {
+    $('.popup-container > input[type="text"]').val('');
+  };
+
+  $('.close').on('click', function () {
+    clearForm();
+    closePopup();
+  });
+});
